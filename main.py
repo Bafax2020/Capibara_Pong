af2da165a9abb4d0b8de9565272d027d88aed970
Bafax2010 <130228641+Bafax2020@users.noreply.github.com>
2023-04-22 15:50:42 +0300

    Bafax2010
    добавлен мяч(нет, капибара)
    сделали ему авто перемещение и отталкивание от нижней и верхней грани
    сделали отталкивание от ракеток
    добавили проигрыши
    
    и написали много кода
    
    ИСПРАВИЛИ
    отрисовка ракет при взлёте и посадке

--- main.py
+++ main.py
@@ -48,6 +48,15 @@ end = False
 #спрайты и тэкст
 racketa1 = Player("rak.png",13,250,3,50,120)
 racketa2 = Player("rak.png",640,250,3,50,120)
+ball = GameSprite("ball.png",100, 100, 0, 50, 50)
+
+speeeed_x = 2
+speeeed_y = 2
+
+font.init()
+
+Loser1 = font.SysFont("Arial", 30).render("Ты проиграл, 1 игрок!", True, (255,0,0))
+Loser2 = font.SysFont("Arial", 30).render("Ты проиграл, 2 игрок!", True, (255,0,0))
 
 while game:
     for e in event.get():
@@ -56,11 +65,24 @@ while game:
 
 
     if end != True:
+        ball.rect.x += speeeed_x
+        ball.rect.y += speeeed_y
+        window.fill(BACKGROUND)
+        ball.reset()
         racketa1.update_L()
         racketa2.update_R()
         racketa1.reset()
         racketa2.reset()
-
+    if ball.rect.y > WinY - 50 or ball.rect.y < 0:
+        speeeed_y *= -1
+    if sprite.collide_rect(racketa1 , ball) or sprite.collide_rect(racketa2 , ball):
+        speeeed_x *= -1
+    if ball.rect.x < 0:
+        window.blit(Loser1,(WinX // 2 - 250, WinY // 2))
+        end = True
+    if ball.rect.x > WinX:
+        window.blit(Loser2,(WinX // 2, WinY // 2))
+        end = True
 
     display.update()
     clock.tick(FPS)
